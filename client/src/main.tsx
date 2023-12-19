c3daac5c89834098816e826ebdd6665cd1d59f3d
Ryan Horrmann <ryanhorrmann123@gmail.com>
2023-12-19 10:24:05 +0100

    Sign up / Sign in UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+import HomeScreen from './screens/HomeScreen.tsx'
+import LoginScreen from './screens/LoginScreen.tsx'
+import RegisterScreen from './screens/RegisterScreen.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
